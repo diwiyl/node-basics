a3a298ab4f5d07b62ca01a6e05fc4993fd4417d4
Rob Hess <robwhess@gmail.com>
2019-05-10 09:55:02 -0700

    Add code from 5/10 lecture.

--- hello.js
+++ hello.js
@@ -16,3 +16,23 @@ figlet(process.env.SPECIAL_MESSAGE, function (err, data) {
     console.log(data);
   }
 });
+
+var fs = require('fs');
+
+fs.readFile('./lib/circle.js', 'utf8', function (err, contents) {
+  if (err) {
+    throw err;
+  }
+  console.log("== File contents:", contents);
+});
+console.log("== This call comes after fs.readFile()");
+
+var contents = fs.readFileSync('./lib/circle.js', 'utf8');
+console.log("== Synchronous file contents:", contents);
+
+fs.writeFile('./hello.txt', "Hello world, for the second time!\n", function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("== File contents written.");
+});
